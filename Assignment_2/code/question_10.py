54ba8bcd06de7530a172c23b40f650b8befef52a
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-03-02 00:56:08 -0500

    Create question_10.py

---
+++ Assignment_2/code/question_10.py
@@ -0,0 +1,14 @@
+# KNN Regression for real data
+def KNN_reg_real(X_big_trn, y_big_trn, X_big_val, y_big_val):
+    K = [1, 2, 5, 10, 20, 50]
+    sq_errs = []
+    for i in K:
+        neigh = KNeighborsRegressor(n_neighbors=i)
+        neigh.fit(X_big_trn, y_big_trn)
+        sq_errs.append([i, mean_squared_error(y_big_trn, neigh.predict(X_big_trn)), mean_squared_error(y_big_val, neigh.predict(X_big_val))])
+    df = pd.DataFrame(np.array(sq_errs),columns=['K', 'Training Data Error', 'Test Data Error'])
+    return df
+  
+df = KNN_reg_real(X_big_trn, y_big_trn, X_big_val, y_big_val)
+print(df)
+df.plot(x="K", title="MSE for KNN Regression on Big Data");
