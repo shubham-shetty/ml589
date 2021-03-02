aac9a2f830d891e5e49dea569de88a7284d0bd08
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-03-02 00:53:51 -0500

    Create question_5.py

---
+++ Assignment_2/code/question_5.py
@@ -0,0 +1,29 @@
+# MSE for Linear Ridge Regression
+def linear_reg_sq_error(X_trn, y_trn, X_val, y_val):
+    l = [0, 0.001, 0.01, 0.1, 1, 10]
+    eval_sq_error = []
+    for i in l:
+        linear_reg_trn = linear_reg_predict(X_trn, linear_reg_train(X_trn, y_trn, i))
+        linear_reg_tst = linear_reg_predict(X_val, linear_reg_train(X_trn, y_trn, i))
+        eval_sq_error.append([i, sq_error(linear_reg_trn, y_trn), sq_error(linear_reg_tst,y_val)])
+    df = pd.DataFrame(np.array(eval_sq_error),columns=['Lambda (l)', 'Training Data Error', 'Test Data Error'])
+    return(df)
+
+df = linear_reg_sq_error(X_trn, y_trn, X_val, y_val)
+print(df)
+df.plot(x="Lambda (l)", title="MSE for Linear Ridge Regression");
+
+# MAE for Linear Ridge Regression
+def linear_reg_abs_error(X_trn, y_trn, X_val, y_val):
+    l = [0, 0.001, 0.01, 0.1, 1, 10]
+    eval_abs_error = []
+    for i in l:
+        linear_reg_trn = linear_reg_predict(X_trn, linear_reg_train(X_trn, y_trn, i))
+        linear_reg_tst = linear_reg_predict(X_val, linear_reg_train(X_trn, y_trn, i))
+        eval_abs_error.append([i, abs_error(linear_reg_trn, y_trn), abs_error(linear_reg_tst,y_val)])
+    df = pd.DataFrame(np.array(eval_abs_error),columns=['Lambda (l)', 'Training Data Error', 'Test Data Error'])
+    return(df)
+  
+df = linear_reg_abs_error(X_trn, y_trn, X_val, y_val)
+print(df)
+df.plot(x="Lambda (l)", title="MAE for Linear Ridge Regression");
