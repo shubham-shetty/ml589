1e319ea6448dde2f7799c184ca040b128b844581
adarshkolya <64338205+adarshkolya@users.noreply.github.com>
2021-03-31 11:13:13 -0400

    Updating q17
    Version of q17 that ran for 6 hours

--- Assignment_3/code/question_17.py
+++ Assignment_3/code/question_17.py
@@ -1,9 +1,13 @@
 from question_16 import *
+from preface import *
 from datetime import datetime
 import pandas as pd
 import sys
+import matplotlib.pyplot as plt
+import math
+import multiprocessing
 
-def gradient_descent_train() :
+def gradient_descent_train(M) :
     data = np.load("data.npz")
     X = data["X_trn"]
     Y = data["y_trn"]
@@ -11,8 +15,8 @@ def gradient_descent_train() :
     momentum = 0.1
     l = 1
     stepSize = 0.0001
-    M = int(sys.argv[1])
-
+    errors = []
+    numIter = 1000
     start = datetime.now()
     start_string = start.strftime("%d/%m/%Y %H:%M:%S")
     print(f"Time - {start_string}. Starting descent for M={M} now")
@@ -20,10 +24,8 @@ def gradient_descent_train() :
     b = np.zeros(M)
     c = np.zeros(numLabels)
     V = np.random.normal(0,1,(numLabels, M))
-    l = 1
     avg_dLdW = avg_dLdV = avg_dLdb = avg_dLdc = 0
-    errors = []
-    for i in range(50) :
+    for i in range(numIter) :
         # ave_grad = (1 - momentum) * ave_grad + momentum * ∇h(w)
         dLdW, dLdV, dLdb, dLdc = prediction_grad_autograd_full(X,Y,W,V,b,c,l)
         avg_dLdW = (1 - momentum) * avg_dLdW + momentum * dLdW
@@ -37,12 +39,31 @@ def gradient_descent_train() :
         b = b - stepSize * avg_dLdb
         c = c - stepSize * avg_dLdc
 
-        errors.append([i, prediction_loss_full(X, Y, W, V, b, c, l)])
+        errors.append(prediction_loss_full(X, Y, W, V, b, c, l))
+
+        if i % 100 == 0 :
+            print(f"{M} --> Iteration {i} done")
         
     end = datetime.now()
-    print(f"Total time taken for M = {M} is {end - start}")
-    df = pd.DataFrame(np.array(errors),columns=['Iterations', 'Regularized loss'])
-    df.plot(x="Iterations", title="Regularized loss vs Iterations")
+    diff = end - start
+    elapsed = int((diff.seconds * 1000) + (diff.microseconds / 1000))
 
-print("Hello")
-gradient_descent_train()
+    print(f"Total time taken for M = {M} is {diff}")
+    return elapsed, errors
+
+if __name__ == '__main__':
+    pool = multiprocessing.Pool(processes=3)
+    result_list = pool.map(gradient_descent_train, [5, 40, 70])
+    
+    runTime = []
+    runTime.append([5, result_list[0][0]])
+    runTime.append([40, result_list[1][0]])
+    runTime.append([70, result_list[2][0]])
+
+    prettyPrintTable(runTime, ["M","Run time in ms"])
+
+    a = np.arange(1000)
+    errors = np.array([a, result_list[0][1], result_list[1][1], result_list[2][1]]).T
+    df = pd.DataFrame(np.array(errors),columns=['Iterations', 'Regularized loss for M=5', 'Regularized loss for M=50', 'Regularized loss for M=70'])
+    df.plot(x="Iterations", title="Regularized loss vs Iterations")
+    plt.savefig('q17.png')
