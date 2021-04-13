d85a3dfd8bd353140b3318b47e5eef4afaeba118
adarshkolya <64338205+adarshkolya@users.noreply.github.com>
2021-04-13 10:19:41 -0400

    Adding legends

--- Assignment_4/code/question_12.py
+++ Assignment_4/code/question_12.py
@@ -17,11 +17,11 @@ for P in [1, 2, 3, 5, 10]:
         Y.append(eval_kernel_ridge(X_trn, X_trn[i], a, k))
     Y = np.array(Y)
 
-    plt.scatter(X_trn, Y_trn)
-    plt.scatter(X_trn, Y)
+    plt.scatter(X_trn, Y_trn, label="Training data")
+    plt.scatter(X_trn, Y, label="Learned function prediction")
+    plt.legend(loc="upper left")
     plt.xlim([0, 15])
     plt.xlabel("x")
     plt.ylabel("y")
     plt.xticks(np.arange(0, 16, 1))
     plt.show()
-    
