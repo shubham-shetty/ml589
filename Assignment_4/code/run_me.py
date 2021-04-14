400124fb3fdcd78a84cbe454e6347b4006e234b1
Brinda M <brinda.m95@gmail.com>
2021-04-14 18:28:00 -0400

    Added solution to q15 q16

--- Assignment_4/code/run_me.py
+++ Assignment_4/code/run_me.py
@@ -3,3 +3,11 @@ import question_7
 
 # Kernel expanded ridge regression for various values of P
 import question_12
+
+from question_15 import compute_hinge_loss
+
+# Estimated hinge loss for P=3
+compute_hinge_loss(3)
+
+# Estimated hinge loss for P=5
+compute_hinge_loss(5)
