479c5330553298ac94e11e922eb2f6c571317de5
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-04-16 01:52:27 -0400

    Updates
    1 - Wrapped code in a function
    2 - Added linspace 
    3 - Plot formatting
    4 - Added comments

--- Assignment_4/code/question_12.py
+++ Assignment_4/code/question_12.py
@@ -6,22 +6,29 @@ from question_8 import get_poly_kernel
 from question_10 import train_kernel_ridge
 from question_11 import eval_kernel_ridge
 
-# fixme 10 - singular matrix error
-for P in [1, 2, 3, 5, 10]:
-    k = get_poly_kernel(P)
-    a = train_kernel_ridge(X_trn, Y_trn, 0.1, k)
-    print(f"P: {P}\n")
-
-    Y = []
-    for i in range(len(X_trn)):
-        Y.append(eval_kernel_ridge(X_trn, X_trn[i], a, k))
-    Y = np.array(Y)
-
-    plt.scatter(X_trn, Y_trn, label="Training data")
-    plt.scatter(X_trn, Y, label="Learned function prediction")
-    plt.legend(loc="upper left")
-    plt.xlim([0, 15])
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.xticks(np.arange(0, 16, 1))
-    plt.show()
+def kernel_ridge_reg():
+    for P in [1, 2, 3, 5, 10]:
+        # Define polynomial kernel function
+        k = get_poly_kernel(P)
+        
+        # Train Kernel Ridge Regression Model
+        a = train_kernel_ridge(X_trn, Y_trn, 0.1, k)
+        print(f"P: {P}")
+        Y = []
+        X_new = np.linspace(0,15,200)
+        
+        # Evaluate for new data
+        for X in X_new:
+            Y.append(eval_kernel_ridge(X_trn, X, a, k))
+        Y = np.array(Y)
+        
+        # Plot results
+        plt.figure(1, figsize=(6, 4))
+        plt.scatter(X_trn, Y_trn, s=20, label="Training Output")
+        plt.scatter(X_new, Y, s=10, label="Learned Function Output")
+        plt.xlim([0, 15])
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.xticks(np.arange(0, 16, 1))
+        plt.legend()
+        plt.show()
