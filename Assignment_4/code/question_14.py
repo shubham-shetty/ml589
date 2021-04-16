ca91fb863104d66a6cb8b6a966f09cdd4785ceea
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-04-16 02:02:35 -0400

    Minor changes

--- Assignment_4/code/question_14.py
+++ Assignment_4/code/question_14.py
@@ -10,7 +10,7 @@ def train_svm(X_trn, y_trn):
     print("Lambda\t\tHinge Loss\n")
     for l in l_vals:
         clf = svm.SVC(kernel='linear', C=1 / (2 * l))
-        sum_hinge_loss = 0
+        sum_hinge_loss, avg_hinge_loss = 0, 0
         for train_index, test_index in kf.split(X_trn):
             # Split train-test
             X_train, X_test = X_trn[train_index], X_trn[test_index]
@@ -21,5 +21,5 @@ def train_svm(X_trn, y_trn):
             predictions = clf.decision_function(X_test)
 
             sum_hinge_loss += hinge_loss(y_test, predictions)
-
-        print(f"{l}\t\t{sum_hinge_loss / splits}")
+        avg_hinge_loss = sum_hinge_loss / splits 
+        print(f"{l}\t\t{avg_hinge_loss}")
