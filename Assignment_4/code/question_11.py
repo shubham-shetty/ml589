f6a193cbd4a82c7c957c5ea3765ede991b5daa0a
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-04-16 01:50:34 -0400

    Optimised version

--- Assignment_4/code/question_11.py
+++ Assignment_4/code/question_11.py
@@ -1,11 +1,5 @@
 from preface import *
 
-def eval_kernel_ridge(X_trn, x, a, k) :
-    N = X_trn.shape[0]
-    y = 0
-
-    # y = sum(a_n * k(X_trn[n],x)).
-    for n in range(N) :
-        y += a[n] * k(X_trn[n], x)
-
+def eval_kernel_ridge(x_trn, x, a, k) :
+    y = np.dot(a, np.array(list(map(lambda x1: k(x1, x), x_trn))))
     return y
