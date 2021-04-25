482df93dfbbfda89028ff07bb93df1f728adda5e
adarshkolya <64338205+adarshkolya@users.noreply.github.com>
2021-04-25 13:39:29 -0400

    Enhanced output

--- Assignment_5/code/question_10.py
+++ Assignment_5/code/question_10.py
@@ -3,5 +3,5 @@ from question_9 import MAP
 from question_7 import posterior
 
 m = MAP(X,Y)
-print(m)
-print(posterior(X,Y,m))
+print(f"MAP estimate m_MAP = {m}")
+print(f"Posterior prob for m_MAP = {posterior(X,Y,m)}")
