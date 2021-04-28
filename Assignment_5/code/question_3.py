a3ce351ab58608e8865e2f144bd7989d82030d34
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-04-27 20:48:13 -0400

    Update question_3.py

--- Assignment_5/code/question_3.py
+++ Assignment_5/code/question_3.py
@@ -1,8 +1,6 @@
-from scipy.stats import norm
-
+from preface import *
 
 def likelihood_single(x, y, m):
     mu = 0 if m == 0 else x ** m
     sigma = 0.1
-    # return (1 / (2 * math.pi * sigma)) * math.exp(-1 * ((y - mu) ** 2) / (2 * (sigma ** 2)))
     return norm.pdf(y, mu, sigma)
