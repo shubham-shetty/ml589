f9c01af1d08260c9fc3e36e36082182bbee66ef9
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-04-27 20:45:45 -0400

    Update preface.py

--- Assignment_5/code/preface.py
+++ Assignment_5/code/preface.py
@@ -1,4 +1,23 @@
+# Import Statements
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy.stats import norm
 
+
+# Load Data
+
+## Training Data
 X = np.loadtxt('x.csv')
 Y = np.loadtxt('y.csv')
+
+## Test Data
+x = np.loadtxt('x_test.csv')
+y = np.loadtxt('y_test.csv')
+
+
+# Predictor Function
+def f_m(x,m):
+    if m>0:
+        return x**m
+    else:
+        return 0
