648e1fb1fa6fedf4d2ce9aee32343e118b84a3af
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-04-27 21:02:11 -0400

    Added q12,13,15,16

--- Assignment_5/code/run_me.py
+++ Assignment_5/code/run_me.py
@@ -9,3 +9,15 @@ import question_8
 
 print("\n\nAnswer 10")
 import question_10
+
+print("\n\nAnswer 12")
+import question_12
+
+print("\n\nAnswer 13")
+import question_13
+
+print("\n\nAnswer 15")
+import question_15
+
+print("\n\nAnswer 16")
+import question_16
