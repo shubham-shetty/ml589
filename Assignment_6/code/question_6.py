49a380a271a86a29038b1e14fbc341e3eee95f7b
adarshkolya <64338205+adarshkolya@users.noreply.github.com>
2021-05-06 22:07:41 -0400

    Update runme
    Update runme

--- Assignment_6/code/question_6.py
+++ Assignment_6/code/question_6.py
@@ -8,13 +8,10 @@ def compute_compression_rate(data):
     compression_rate = []
     for k in [3, 5, 10, 30, 50, 100]:
         dim_reduced_face_data, eigen_vectors = pca_dim_reduction(data, k)
-        dim_reduced_face_data = dim_reduced_face_data.astype(np.float64)
-        eigen_vectors = eigen_vectors.astype(np.float64)
-
-        original_data_size = np.float64(data.size * data.itemsize)
+        original_data_size = data.size * data.itemsize
         compressed_data_size = dim_reduced_face_data.size * dim_reduced_face_data.itemsize + eigen_vectors.size * eigen_vectors.itemsize
-        compression_rate.append((k, np.float64(compressed_data_size / original_data_size)))
+        compression_rate.append((k, compressed_data_size / original_data_size))
     return compression_rate
 
-
+print(f"Table showing the compression rate for each value of k using PCA")
 prettyPrintTable(np.array(compute_compression_rate(face_data)), ['k', 'Compression rate'])
